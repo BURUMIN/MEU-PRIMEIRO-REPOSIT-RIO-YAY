d782de62c053961f78d3e2b76d781a2233152373
gabrielfdf15 <gabrielfdf15@gmail.com>
2026-04-17 16:10:23 -0300

    pqp

--- Assets/LixoSpawnerController.cs
+++ Assets/LixoSpawnerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class LixoSpawnerController : MonoBehaviour
 {
@@ -11,6 +12,8 @@ public class LixoSpawnerController : MonoBehaviour
     public GameObject Lixo;
     public int maxPonto;
     public int pontos = 0;
+    public TMP_Text textoDoPonto;
+    public TMP_Text textoDeVitoria;
     // Start is called before the first frame update
     IEnumerator SpawnRoutine() {
         while (pontos < maxPonto)
@@ -22,10 +25,20 @@ public class LixoSpawnerController : MonoBehaviour
                     Quaternion.identity);
             yield return new WaitForSeconds(timer);
         }
+        textoDeVitoria.gameObject.SetActive(true);
     }
+    
+    public void AddToPoints (int value)
+    {
+        pontos += value;
+        textoDoPonto.text = "Pontos: "+pontos.ToString();
+    }
+
     void Start()
     {
         StartCoroutine(SpawnRoutine());
+        textoDeVitoria.gameObject.SetActive(false);
+        textoDoPonto.text="Pontos: 0";
     }
 
     // Update is called once per frame
---
+++ Assets/Ponto.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ponto : MonoBehaviour
+{
+    public LixoSpawnerController LixoSpawnerController;
+
+    private void OnCollisionEnter(Collision collision) {
+        if(collision.gameObject.CompareTag("Lixo")) {
+            Destroy(collision.gameObject);
+            LixoSpawnerController.AddToPoints(1);
+        }
+        
+    }
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
